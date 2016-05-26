3581e14eae6843b95882352cc44e169b444298ae
Rishabh Jaiswal <rishabhj1024@gmail.com>
2016-05-26 23:27:20 +0530

    Update sol25.js
    palindrome

--- sol25.js
+++ sol25.js
@@ -1,4 +1,5 @@
-
+//help taken from Rafase282 repository
+//task is really amazing
 function palindrome(str) {
   var nStr = str.replace(/[\W_]/g, '').toLowerCase();
   var rStr = nStr.split('').reverse().join('');
