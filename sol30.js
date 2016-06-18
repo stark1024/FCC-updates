645bc01e58cd5c17b12e13c374650d4f1a1f554a
Rishabh Jaiswal <rishabhj1024@gmail.com>
2016-06-19 00:18:17 +0530

    Update sol30.js
    TitleCase

--- sol30.js
+++ sol30.js
@@ -1,12 +1,8 @@
-
-function titleCase(str) {
   var aStr = str.toLowerCase().split(" ");
-  var nStr;
-  for(var i=0; i<aStr.ength; i++){
-    aStr[i].split("")[0].toUpperCase();
+  var nStr = [];
+  for(var i=0; i<aStr.length; i++){
+    aStr[i].split("")[0] = aStr[i].split("")[0].toUpperCase();
+    //nStr.push(aStr);
   }
   str = aStr.join(" ");
   return str;
-}
-
-titleCase("I'm a little tea pot");
